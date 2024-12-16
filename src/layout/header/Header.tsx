ab9fa6c8f873bae52d23f70e58030e442be7a2e1
qasemB <mahdicmptr@gmail.com>
2024-12-16 18:13:30 +0330

    part-C10-react_icons

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,14 +1,22 @@
+import { RxHamburgerMenu } from "react-icons/rx";
 import { useAppDispatch } from "../../redux/reduxHooks";
 import { setShowSidebar } from "../../redux/ui-management/uiManagement";
 
 const Header = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   return (
     <section
       id="header"
-      className="fixed top-0 left-0 h-app_header_h w-full bg-white dark:bg-gray-700 md:pr-app_sidebar_w shadow-lg"
+      className="fixed top-0 left-0 h-app_header_h w-full bg-white dark:bg-gray-700 md:pr-app_sidebar_w shadow-lg p-2"
     >
-      <button onClick={()=>dispatch(setShowSidebar(true))}>نمایش سایدبار</button>
+      <div className="flex items-center h-full">
+        <button
+          onClick={() => dispatch(setShowSidebar(true))}
+          className="md:hidden"
+        >
+          <RxHamburgerMenu size={24} />
+        </button>
+      </div>
     </section>
   );
 };
--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,23 +1,35 @@
+import { IoCloseOutline, IoSunnyOutline, IoMoon } from "react-icons/io5";
 import { useAppDispatch, useAppSelector } from "../../redux/reduxHooks";
-import { setShowSidebar, toggleTheme } from "../../redux/ui-management/uiManagement";
+import {
+  setShowSidebar,
+  toggleTheme,
+} from "../../redux/ui-management/uiManagement";
 
 const Sidebar = () => {
-  const dispatch = useAppDispatch()
-  const {showSidebar} = useAppSelector(state=>state.uiManagerReducer)
+  const dispatch = useAppDispatch();
+  const { showSidebar, theme } = useAppSelector(
+    (state) => state.uiManagerReducer
+  );
   return (
     <section
       id="sidebar"
-      className={`fixed lg:right-0 top-0 w-app_sidebar_w h-screen bg-white dark:bg-gray-700  md:block transition-all border-l border-gray-500 ${
+      className={`p-3 fixed lg:right-0 top-0 w-app_sidebar_w h-screen bg-white dark:bg-gray-700  md:block transition-all border-l border-gray-500 ${
         showSidebar ? "right-0" : "-right-app_sidebar_w"
       }`}
     >
-      سایدبار
-      <button className="block" onClick={()=>dispatch(setShowSidebar(false))}>
-        بستن منو
-      </button>
-      <button className="block" onClick={()=>dispatch(toggleTheme())}>
-        دارک مود
-      </button>
+      <div className="h-full w-full">
+        <div className="flex justify-between items-center">
+          <button
+            className="block"
+            onClick={() => dispatch(setShowSidebar(false))}
+          >
+            <IoCloseOutline size={24} />
+          </button>
+          <button className={`block transform transition-all ${theme === "dark" && "rotate-45"}`} onClick={() => dispatch(toggleTheme())} >
+            {theme === "dark" ? <IoSunnyOutline size={24}/> : <IoMoon size={24}/>}
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
