e4537924ca3ab00bacfaa179b5e86ed7a7bab1c2
qasemB <mahdicmptr@gmail.com>
2025-01-01 16:36:14 +0330

    part-C16-creating_table-defining_data_types

--- src/page/categories/Categories.tsx
+++ src/page/categories/Categories.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { getTaskCategoriesService } from "../../services/taskCategory";
+import { CategoryListItemType } from "../../types/taskCategory";
 
 const Categories = () => {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState<CategoryListItemType[]>([]);
   const handleGetTaskCategories = async () => {
     const data = await getTaskCategoriesService();
     setCategories(data);
@@ -12,9 +13,31 @@ const Categories = () => {
   }, []);
   return (
     <div>
-      {categories.map((cat: { id: string }) => (
-        <div>{cat.id}</div>
-      ))}
+      <table className="table w-full rounded-lg overflow-hidden shadow-sm bg-white dark:bg-gray-600 dark:shadow-gray-500">
+        <thead>
+          <tr className="border-b h-12 [&>th]:px-2 [&>th]:md:px-3 [&>th]:text-center">
+            <th className=" hidden md:table-cell">#</th>
+            <th>عنوان</th>
+            <th>توضیحات</th>
+            <th>تاریخ ایجاد</th>
+            <th>عملیات</th>
+          </tr>
+        </thead>
+        <tbody className="md:text-3 text-gray-800 dark:text-gray-400">
+          {categories.map((item) => (
+            <tr
+              key={item.id}
+              className="h-9 border-b border-dashed dark:border-grashadow-gray-500 [&>td]:px-2 [&>td]:md:px-3 [&>td]:text-center"
+            >
+              <td className=" hidden md:table-cell"> {item.id} </td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>{item.createdAt}</td>
+              <td>Action</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
---
+++ src/types/taskCategory.d.ts
@@ -0,0 +1,8 @@
+export type CategoryListItemType = {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  userId: string;
+  createdAt: string;
+};
